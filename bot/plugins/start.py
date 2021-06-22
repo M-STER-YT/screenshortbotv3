26dae7d81d1765402823c36f9522c3c8c1d405fd
M-STER <86182000+M-STER-YT@users.noreply.github.com>
2021-06-22 08:56:47 -0700

    verthe oru rasam
    onulla verthe keranda

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -20,12 +20,12 @@ async def start(c, m):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('📌  Support Group', url='https://t.me/InFoTelGroup'),
-                    InlineKeyboardButton('🔖  Projects Channel', url='https://t.me/TGBotsZ')
+                    InlineKeyboardButton('📌  Support Group', url='https://t.me/M_SRER_TECH_GROUP'),
+                    InlineKeyboardButton('🔖  Projects Channel', url='https://t.me/M_STER_TECH')
                 ],
                 [
                     InlineKeyboardButton('💡  Source Code', url='https://github.com/TGExplore/Screenshot-Bot'),
-                    InlineKeyboardButton('👨  Master', url='https://t.me/odbots')
+                    InlineKeyboardButton('👨  Master', url='https://t.me/CYBER_YT1')
                 ]
             ]
         )
